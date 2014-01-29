943986faaf048c1c86e4829b7b2a04f1be03b782
yoppi <y.hirokazu@gmail.com>
2014-01-29 10:30:35 +0900

    go fmt

--- api.go
+++ api.go
@@ -3,30 +3,30 @@ package gochatwork
 const BaseUrl = `https://api.chatwork.com/v1`
 
 func (c *Client) Me() string {
-        return c.Get("/me", map[string]string{})
+	return c.Get("/me", map[string]string{})
 }
 
 func (c *Client) MyStatus() string {
-        return c.Get("/my/status", map[string]string{})
+	return c.Get("/my/status", map[string]string{})
 }
 
 // params keys
 //  - assigned_by_account_id
 //  - status: [open, done]
 func (c *Client) MyTasks(params map[string]string) string {
-        return c.Get("/my/tasks", params)
+	return c.Get("/my/tasks", params)
 }
 
 func (c *Client) Contacts() string {
-        return c.Get("/contacts", map[string]string{})
+	return c.Get("/contacts", map[string]string{})
 }
 
 func (c *Client) Rooms() string {
-        return c.Get("/rooms", map[string]string{})
+	return c.Get("/rooms", map[string]string{})
 }
 
 func (c *Client) Room(roomId string) string {
-        return c.Get("/rooms/" + roomId, map[string]string{})
+	return c.Get("/rooms/"+roomId, map[string]string{})
 }
 
 // params keys
@@ -37,36 +37,36 @@ func (c *Client) Room(roomId string) string {
 //   - members_member_ids
 //   - members_readonly_ids
 func (c *Client) CreateRoom(params map[string]string) string {
-        return c.Post("/rooms", params)
+	return c.Post("/rooms", params)
 }
 
 func (c *Client) RoomMembers(roomId string) string {
-        return c.Get("/rooms/" + roomId + "/members", map[string]string{})
+	return c.Get("/rooms/"+roomId+"/members", map[string]string{})
 }
 
 // XXX: Not yet implement
 func (c *Client) RoomMessages(roomId string) string {
-        return c.Get("/rooms/" + roomId + "/messages", map[string]string{})
+	return c.Get("/rooms/"+roomId+"/messages", map[string]string{})
 }
 
 func (c *Client) RoomMessage(roomId, messageId string) string {
-        return c.Get("/rooms/" + roomId + "/messages/" + messageId, map[string]string{})
+	return c.Get("/rooms/"+roomId+"/messages/"+messageId, map[string]string{})
 }
 
 func (c *Client) RoomTasks(roomId string) string {
-        return c.Get("/rooms/" + roomId + "/tasks", map[string]string{})
+	return c.Get("/rooms/"+roomId+"/tasks", map[string]string{})
 }
 
 func (c *Client) RoomTask(roomId, taskId string) string {
-        return c.Get("/rooms/" + roomId + "/tasks/" + taskId, map[string]string{})
+	return c.Get("/rooms/"+roomId+"/tasks/"+taskId, map[string]string{})
 }
 
 // params key
 //   - account_id
 func (c *Client) RoomFiles(roomId string, params map[string]string) string {
-        return c.Get("/rooms/" + roomId + "/files", params)
+	return c.Get("/rooms/"+roomId+"/files", params)
 }
 
 func (c *Client) RoomFile(roomId, fileId string) string {
-        return c.Get("/rooms/" + roomId + "/files/" + fileId, map[string]string{})
+	return c.Get("/rooms/"+roomId+"/files/"+fileId, map[string]string{})
 }
--- client.go
+++ client.go
@@ -2,53 +2,52 @@ package gochatwork
 
 import (
 	"bytes"
-        "net/http"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/url"
-        "strings"
-        "io/ioutil"
-        "log"
+	"strings"
 )
 
 type Http interface {
-        Get()
-        Post()
-        Put()
-        Delete()
+	Get()
+	Post()
+	Put()
+	Delete()
 }
 
 type Client struct {
-        ApiKey string
-        BaseUrl string
-        Http
+	ApiKey  string
+	BaseUrl string
+	Http
 }
 
 func NewClient(apiKey string) *Client {
-        return &Client{ApiKey: apiKey, BaseUrl: BaseUrl}
+	return &Client{ApiKey: apiKey, BaseUrl: BaseUrl}
 }
 
 func (c *Client) Get(endpoint string, params map[string]string) string {
-        return c.execute("GET", endpoint, params)
+	return c.execute("GET", endpoint, params)
 }
 
 func (c *Client) Post(endpoint string, params map[string]string) string {
-        return c.execute("POST", endpoint, params)
+	return c.execute("POST", endpoint, params)
 }
 
 func (c *Client) Put(endpoint string, params map[string]string) string {
-        return c.execute("PUT", endpoint, params)
+	return c.execute("PUT", endpoint, params)
 }
 
-
 func (c *Client) Delete(endpoint string, params map[string]string) string {
-        return c.execute("DELETE", endpoint, params)
+	return c.execute("DELETE", endpoint, params)
 }
 
 func (c *Client) buildUrl(baseUrl, endpoint string, params map[string]string) string {
-        query := make([]string, len(params))
-        for k := range params {
-                query = append(query, k + "=" + params[k])
-        }
-        return baseUrl + endpoint + "?" + strings.Join(query, "&")
+	query := make([]string, len(params))
+	for k := range params {
+		query = append(query, k+"="+params[k])
+	}
+	return baseUrl + endpoint + "?" + strings.Join(query, "&")
 }
 
 func (c *Client) buildBody(params map[string]string) url.Values {
@@ -61,39 +60,39 @@ func (c *Client) buildBody(params map[string]string) url.Values {
 
 func (c *Client) parseBody(resp *http.Response) string {
 	defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                log.Println(err)
-                return ""
-        }
-        return string(body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return string(body)
 }
 
 func (c *Client) execute(method, endpoint string, params map[string]string) string {
-        httpClient := &http.Client{}
+	httpClient := &http.Client{}
 
 	var (
-		req *http.Request
+		req        *http.Request
 		requestErr error
 	)
 
-        if method != "GET" {
-		req, requestErr = http.NewRequest(method, c.BaseUrl + endpoint, bytes.NewBufferString(c.buildBody(params).Encode()))
+	if method != "GET" {
+		req, requestErr = http.NewRequest(method, c.BaseUrl+endpoint, bytes.NewBufferString(c.buildBody(params).Encode()))
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-        } else {
-                req, requestErr = http.NewRequest(method, c.buildUrl(c.BaseUrl, endpoint, params), nil)
-        }
-        if requestErr != nil {
-                panic(requestErr)
-        }
+	} else {
+		req, requestErr = http.NewRequest(method, c.buildUrl(c.BaseUrl, endpoint, params), nil)
+	}
+	if requestErr != nil {
+		panic(requestErr)
+	}
 
-        req.Header.Add("X-ChatWorkToken", c.ApiKey)
+	req.Header.Add("X-ChatWorkToken", c.ApiKey)
 
-        resp, err := httpClient.Do(req)
-        if err != nil {
-                log.Println(err)
-                return ""
-        }
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
 
-        return c.parseBody(resp)
+	return c.parseBody(resp)
 }
