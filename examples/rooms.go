2a278c1f3dee758a3db8f35cad0b1c026bdeff09
yoppi <y.hirokazu@gmail.com>
2014-02-08 20:55:01 +0900

    Fix example code

--- examples/rooms.go
+++ examples/rooms.go
@@ -2,28 +2,50 @@ package main
 
 import (
         chatwork "github.com/yoppi/go-chatwork"
+	"flag"
         "fmt"
 )
 
-var apiKey = `api-key`
+var apiKey string
+
+func init() {
+	flag.StringVar(&apiKey, "key", "", "Chatwork API key")
+	flag.Parse()
+}
 
 func main() {
 	// GET
         c := chatwork.NewClient(apiKey)
-        fmt.Print(c.Rooms())
-        fmt.Print(c.Room("room-id"))
-        fmt.Print(c.RoomMembers("room-id"))
-        fmt.Print(c.RoomMessages("room-id"))
-        fmt.Print(c.RoomMessage("room-id", "message-id"))
-        fmt.Print(c.RoomTasks("room-id"))
-        fmt.Print(c.RoomTask("room-id", "task-id"))
-        fmt.Print(c.RoomFiles("room-id", map[string]string{}))
-        fmt.Print(c.RoomFile("room-id", "file-id"))
+        fmt.Printf("%+v\n", c.Rooms())
+        fmt.Printf("%+v\n", c.Room(`room-id`))
+        fmt.Printf("%+v\n", c.RoomMembers(`room-id`))
+        fmt.Print(c.RoomMessages(`room-id`))
+        fmt.Printf("%+v\n", c.RoomMessage(`room-id`, `message-id`))
+        fmt.Printf("%+v\n", c.RoomTasks(`room-id`))
+        fmt.Printf("%+v\n", c.RoomTask(`room-id`, `task-id`))
+        fmt.Printf("%+v\n", c.RoomFiles(`room-id`, map[string]string{}))
+        fmt.Printf("%+v\n", c.RoomFile(`room-id`, `file-id`))
 
 	// POST
-	fmt.Println(c.CreateRoom(map[string]string {
+	c.CreateRoom(map[string]string {
 		"name": "Test Room",
 		"members_admin_ids": `user-id`,
 		"description": "テスト",
-	}))
+	})
+
+	// PUT
+	c.UpdateRoom(`room-id`, map[string]string {
+		"name": "テストルーム",
+		"description": "Update description",
+	})
+	c.UpdateRoomMembers(`room-id`, map[string]string {
+		"members_admin_ids": `user-id`,
+		"members_member_ids": `user-id`,
+		"members_readonly_ids": `user-id`,
+	})
+
+	// DELETE
+	c.DeleteRoom(`room-id`, map[string]string {
+		"action_type": "delete",
+	})
 }
